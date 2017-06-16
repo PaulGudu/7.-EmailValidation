fe6459b2602475f79e8c26265097599397487d7e
Paul Gudu <3348225661@qq.com>
2017-06-16 20:46:35 +0800

    completed email verification

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  test = email.split('@');
+  if (test[1]==suffixes[0]) {
+  	return true;
+  }else {
+  	return false;
+  }
 };
